392281a1c3e179c755822b9e21f4642c8d95a8f7
mtokhi20 <mohammed.tokhi.badr@gmail.com>
2022-04-03 14:11:49 +0200

    V1.1.1

--- wiki/src/App.js
+++ wiki/src/App.js
@@ -2,45 +2,38 @@ import React,{useState,useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap";
 import Cards from "./components/cards/cards";
+
 function App() {
   // let [pageNumber, setPageNumber] = useState(7); 
   let [fetchedData, updateFetchedData] = useState([]); 
-  let { info, results } = fetchedData; 
-  console.log(results);
-
+  let { results } = fetchedData; 
+ console.log(results);
   let api ="https://rickandmortyapi.com/api/character/?page=20";
 
   //Get Data from Api
   useEffect (()=>{
-     (async function (){
+    (async function (){
        let data = await fetch(api).then(res=>res.json());
-       updateFetchedData(data);
-
-     })()
-     
+      updateFetchedData(data);
+    })()     
   },[api]);
+
   return (
     <div className="App">
-
       <h1 className="text-center my-4 text-info">Character Wiki</h1>
-
       <div className="container">
         <div className="row">
-          <div className="col-12">
+          <div className="col-8">
             <div className="row">
-              {/* <div className="col-4">Cards</div>
-              <div className="col-4">Cards</div>
-              <div className="col-4">Cards</div> */}
-              <Cards />
-              <Cards />
-              <Cards />
+              
+              <Cards className="col- 4" results={results} />
+              <Cards className="col- 4" results={results} />
+              <Cards className="col- 4" results={results} />
+              
             </div>
           </div>
         </div>
-
-
       </div>
-
     </div>
   );
 }
--- wiki/src/components/cards/cards.js
+++ wiki/src/components/cards/cards.js
@@ -1,11 +1,32 @@
 import React from "react";
+ import styles from './card.scss';
 
+const Cards = ({results}) => {
+    let display ;
 
-const Cards =()=>{
-
-    return (
-        <div className="col-4"> Cards</div>
-    );
-}
+    if(results){
+       display = results.map(x => {
+           let {id , name, image, location} = x;
+           return(
+               //it might be col-12
+                <div key={id} className="col-4 mb-4 position-relative">
+                    <div className={`${styles.cards}`}>
+                        <img src={image} alt="" className={`${styles.img} img-fluid`} />
+                        <div style={{padding : "10px"}} className="content">
+                            <div className="fs-4 fw-bold mb-4">{name}</div>
+                            <div className="">
+                                <div className="fs-6">Last Location</div>
+                                <div className="fs-5">{location.name}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+       });
+    } else {
+        display= "No Characters Found"
+    }
+    return <div>{display}</div>;
+};
 
 export default Cards
